d264c90ad7756f4db291b1c885787ecf9800e53e
Aroldo Souza-Leite <asouzaleite@gmx.de>
2025-04-09 17:29:21 +0200

    Keine Wohnungssuche mehr.

--- wohnungssuche/wohnungssuche-tabelle-compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-rst=wohnungssuche-tabelle.txt
-tex=wohnungssuche-tabelle.tex
-pdf=wohnungssuche-tabelle.pdf
-stylesheet=+geometry.tex
-pdf_viewer=xreader
-rst2latex.py $rst $tex --stylesheet=$stylesheet
-pdflatex $tex $pdf
-$pdf_viewer $pdf
